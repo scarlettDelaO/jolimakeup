57984eec4da30bb463d9916d659439e8cb367572
scarlettDelaO <scarlettodal12@gmail.com>
2023-11-10 10:42:32 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   app/Http/Controllers/UserController.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function home(){
+        return view('principal');
+    }
+
+    public function login(){
+        return view('login');
+    }
+
+    public function regi(){
+        return view('registro');
+    }
+    public function per(){
+        return view('perfil');
+    }
+
+    public function ojos(){
+        return view('ojos');
+    }
+
+}
