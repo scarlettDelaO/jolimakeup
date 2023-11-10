321d98230637c59131f7a198e9268017781a78b5
scarlettDelaO <scarlettodal12@gmail.com>
2023-11-10 04:51:34 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   public/css propios/estilo_login.css
    	new file:   resources/views/navIR.blade.php

---
+++ resources/views/navIR.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JoliMakeup</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css propios/estilo_login.css">
+    
+    <link rel="stylesheet" type="text/css" href="css propios/estilo_navar rosa.css">
+</head>
+<body>
+<nav id="ir" class="navbar navbar-dark">
+            <!-- NAV CONTAINER START -->
+            <div class="container-fluid">
+                <a href="" class="navbar-brand text-info">
+                    <img src="imagenes/logo1inicio.png" alt="Logo" style="width:150px;" class="ms-lg-4">
+                </a>
+                <div style="width: 70%"></div>
+                <a href="" class="navbar-brand text-info">
+                    <img src="imagenes/bolsa icon.png" alt="Logo" style="width:37px;">
+                </a>
+                
+                <a href="" class="navbar-brand text-info">
+                    <img src="imagenes/user.png" alt="Logo" style="width:37px;">
+                </a>
+            </div>
+    
+        </nav>
+</body>
+</html>
