517125518288675f957eadc025017137b9ed0b53
DairaDV <daira.demillon@gmail.com>
2023-11-09 21:40:17 -0600

    new file:   resources/views/footer.blade.php 	new file:   resources/views/navbarRosa.blade.php 	modified:   resources/views/principal.blade.php 	modified:   routes/web.php

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,14 @@
+@extends('navbarRosa')
+@section('contenidoPrincipal')
+<section class="row">
+      <div id="lateral" class="col-lg-1 mt-lg-5 mb-lg-5"></div>
+      <div id="imagen" class="col-lg-4 img-fluid align-items-lg-center mt-lg-5 mb-lg-5">
+        <img src="imagenes/portada.png" >
+      </div>
+      <div id="texto" class="col-lg-6 mt-lg-5 mb-lg-5 row">
+          <h1 class="pt-lg-5 pb-lg-0 mt-lg-5">Joly <br>Makeup</h1>
+          <p>Descubre tu estilo y resalta tu belleza con nuestra amplia gama de productos de maquillaje de alta calidad. Encuentra los colores y texturas que te inspiran. ¡Comienza tu viaje de belleza hoy!</p>
+      </div>
+      <div id="lateral" class="col-lg-1 mt-lg-5 mb-lg-5"></div>
+</section>
+@extends('footer')    
