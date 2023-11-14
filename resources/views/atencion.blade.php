f42e300205322e812b01661531381425e56ecf89
scarlettDelaO <scarlettodal12@gmail.com>
2023-11-14 01:51:46 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   resources/views/atencion.blade.php

--- resources/views/atencion.blade.php
+++ resources/views/atencion.blade.php
@@ -13,15 +13,15 @@
         <nav class="navbar navbar-dark col-lg-12 col-md-12 col-sm-12 col-12 row m-lg-0 m-md-0 m-sm-0 m-0">
             <!-- NAV CONTAINER START -->
             <div class="row col-lg-12 col-md-12 col-sm-12 col-12 m-lg-0 m-md-0 m-sm-0 m-0">
-            <a href="" class="navbar-brand text-info">
-              <img src="imagenes/icon_casa.png" alt="Logo" style="width:37px;" class="ms-lg-4">
-            </a>
+                <a href="" class="navbar-brand m-0 p-2 col-lg-10 col-md-9 col-sm-8 col-7 m-lg-0 m-md-0 m-sm-0 m-0">
+                    <img src="imagenes/logo1inicio.png" alt="Logo" style="width:180px;" class="ms-lg-4 ms-md-4 ms-sm-4 ms-1">
+                </a>
                 <div class="col-lg-2 row m-0 col-md-3 col-sm-4 col-5">
                     <a href="" class="navbar-brand text-info col-lg-4 col-md-4 col-sm-4 col-4 m-lg-0 m-md-0 m-sm-0 m-0">
-                        <img clas="m-0" src="imagenes/bolsa rosa.png" alt="Logo" style="width:100%;">
+                        <img clas="m-0" src="imagenes/bolsa icon.png" alt="Logo" style="width:100%;">
                     </a>
                     <a  clas="m-0" href="" class="navbar-brand text-info col-lg-4 col-md-4 col-sm-4 col-4 m-lg-0 m-md-0 m-sm-0 m-0">
-                        <img src="imagenes/user_rosa.png" alt="Logo" style="width:100%;">
+                        <img src="imagenes/user.png" alt="Logo" style="width:100%;">
                     </a>
                 <!-- NAV BUTTON  -->
                     <button
@@ -30,7 +30,7 @@
                         data-bs-toggle="offcanvas"
                         data-bs-target="#menuLateral"
                     >
-                        <img clas="m-0" src="imagenes/menu rosa.png" alt="Logo" style="width:100%;">
+                        <img clas="m-0" src="imagenes/icon_menu.png" alt="Logo" style="width:100%;">
                     </button>
                 </div>
               <!-- OFFCANVAS MAIN CONTAINER START -->
@@ -72,6 +72,7 @@
             </div>
         </nav>
     </header>
+
 
 <section id="aten">
 <div class="container">
