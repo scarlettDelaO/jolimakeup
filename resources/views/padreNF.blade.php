ffba3596200dd0a70be29aecc353a906ddd1883e
DairaDV <daira.demillon@gmail.com>
2023-11-14 01:12:18 -0600

    Changes to be committed: 	modified:   resources/views/padreNF.blade.php

--- resources/views/padreNF.blade.php
+++ resources/views/padreNF.blade.php
@@ -78,74 +78,74 @@
     @yield('contenidoPrincipal')
 
     <footer
-          class="text-center text-lg-start text-white"
-          style="background-color: #1D1C1C"
-          >
-    <!-- Grid container -->
-    <div class="container p-4 pb-0">
+            class="text-center text-lg-start text-white"
+            style="background-color: #1D1C1C"
+            >
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
 
-      <section  class="">
-        <p  class="d-flex justify-content-center align-items-center">
-          <span  id="sig" class="me-3"><img src="imagenes/SIGUENOS.png"style="width:100%;"></span>
-        </p>
-      </section>
+        <section  class="">
+            <p  class="d-flex justify-content-center align-items-center">
+            <span  id="sig" class="me-3"><img src="imagenes/SIGUENOS.png"style="width:100%;"></span>
+            </p>
+        </section>
 
-      <!-- Section: Social media -->
-      <section class="mb-2 text-center">
-        <!-- Facebook -->
-        <a
-           class="btn btn-floating "
-           href="#!"
-           role="button"
-           >
-          <img src="imagenes/icon_facebook.png"style="width:90%;">
-          </a>
+        <!-- Section: Social media -->
+        <section class="mb-2 text-center">
+            <!-- Facebook -->
+            <a
+            class="btn btn-floating "
+            href="#!"
+            role="button"
+            >
+            <img src="imagenes/icon_facebook.png"style="width:90%;">
+            </a>
 
-        <!-- Twitter -->
-        <a
-           class="btn btn-floating "
-           href="#!"
-           role="button"
-           ><img src="imagenes/icon_twitterX.png"style="width:90%;">
-          </a>
+            <!-- Twitter -->
+            <a
+            class="btn btn-floating "
+            href="#!"
+            role="button"
+            ><img src="imagenes/icon_twitterX.png"style="width:90%;">
+            </a>
 
-        <!-- Pinterest -->
-        <a
-           class="btn btn-floating "
-           href="#!"
-           role="button"
-           ><img src="imagenes/icon_pinterest.png" style="width:90%;">
-          </a>
+            <!-- Pinterest -->
+            <a
+            class="btn btn-floating "
+            href="#!"
+            role="button"
+            ><img src="imagenes/icon_pinterest.png" style="width:90%;">
+            </a>
 
-        <!-- Tiktok -->
-        <a
-           class="btn btn-floating "
-           href="#!"
-           role="button"
-           ><img src="imagenes/icon_tiktok.png"style="width:90%;">
-          </a>
+            <!-- Tiktok -->
+            <a
+            class="btn btn-floating "
+            href="#!"
+            role="button"
+            ><img src="imagenes/icon_tiktok.png"style="width:90%;">
+            </a>
 
-        <!-- Instagram -->
-        <a
-           class="btn btn-floating "
-           href="#!"
-           role="button"
-           ><img src="imagenes/icon_instagram.png"style="width:90%;">
-          </a>
-      </section>
-      <!-- Section: Social media -->
-    </div>
-    <!-- Grid container -->
+            <!-- Instagram -->
+            <a
+            class="btn btn-floating "
+            href="#!"
+            role="button"
+            ><img src="imagenes/icon_instagram.png"style="width:90%;">
+            </a>
+        </section>
+        <!-- Section: Social media -->
+        </div>
+        <!-- Grid container -->
 
-    <!-- Copyright -->
-    <div class="text-center p-3 row">
-      <p class="col-3">Aviso de privacidad</p>
-      <p class="col-3">Terminos y condiciones</p>
-      <a href="#!" class="col-3"><p>Atencion al cliente</p></a>
-      <p class="col-3">© 2020 Copyright:JM</p>
-    </div>
-    <!-- Copyright -->
-  </footer>
+        <!-- Copyright -->
+        <div class="text-center p-3 row">
+        <p class="col-3">Aviso de privacidad</p>
+        <p class="col-3">Terminos y condiciones</p>
+        <a href="#!" class="col-3"><p>Atencion al cliente</p></a>
+        <p class="col-3">© 2020 Copyright:JM</p>
+        </div>
+        <!-- Copyright -->
+    </footer>
   <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
