bf04b04ac4ca83bcb64525cf0f6342f51aa5f550
scarlettDelaO <scarlettodal12@gmail.com>
2023-11-14 01:49:01 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   public/css propios/estilo_atencion.css
    	new file:   public/imagenes/bolsa rosa.png
    	new file:   public/imagenes/menu rosa.png
    	new file:   public/imagenes/user_rosa.png
    	modified:   resources/views/atencion.blade.php
    	modified:   resources/views/padreN.blade.php

--- resources/views/atencion.blade.php
+++ resources/views/atencion.blade.php
@@ -1,5 +1,79 @@
-<link rel="stylesheet" type="text/css" href="css propios/estilo_atencion.css">
-<section>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JoliMakeup</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css propios/estilo_navar dark.css">
+    <link rel="stylesheet" type="text/css" href="css propios/estilo_atencion.css">
+
+    <body class="row">
+    <header class="row col-lg-12 col-md-12 col-sm-12 col-12 m-lg-0 m-md-0 m-sm-0 m-0 container-fluid">
+        <nav class="navbar navbar-dark col-lg-12 col-md-12 col-sm-12 col-12 row m-lg-0 m-md-0 m-sm-0 m-0">
+            <!-- NAV CONTAINER START -->
+            <div class="row col-lg-12 col-md-12 col-sm-12 col-12 m-lg-0 m-md-0 m-sm-0 m-0">
+            <a href="" class="navbar-brand text-info">
+              <img src="imagenes/icon_casa.png" alt="Logo" style="width:37px;" class="ms-lg-4">
+            </a>
+                <div class="col-lg-2 row m-0 col-md-3 col-sm-4 col-5">
+                    <a href="" class="navbar-brand text-info col-lg-4 col-md-4 col-sm-4 col-4 m-lg-0 m-md-0 m-sm-0 m-0">
+                        <img clas="m-0" src="imagenes/bolsa rosa.png" alt="Logo" style="width:100%;">
+                    </a>
+                    <a  clas="m-0" href="" class="navbar-brand text-info col-lg-4 col-md-4 col-sm-4 col-4 m-lg-0 m-md-0 m-sm-0 m-0">
+                        <img src="imagenes/user_rosa.png" alt="Logo" style="width:100%;">
+                    </a>
+                <!-- NAV BUTTON  -->
+                    <button
+                        class="navbar-toggler col-lg-4 col-md-4 col-sm-4 col-4 m-lg-0 m-md-0 m-sm-0 m-0"
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#menuLateral"
+                    >
+                        <img clas="m-0" src="imagenes/menu rosa.png" alt="Logo" style="width:100%;">
+                    </button>
+                </div>
+              <!-- OFFCANVAS MAIN CONTAINER START -->
+              <section
+                class="offcanvas offcanvas-end row col-12 pe-4"
+                id="menuLateral"
+                tabindex="-1"
+              >
+                <div class="offcanvas-header col-lg-12 col-md-12 col-sm-12 col-12 m-lg-0 m-md-0 m-sm-0 m-0 row">
+                    <div class="col-9 m-0"></div>
+                    <div class=" col-3 m-0 ps-3">
+                        <button class="my-button col-12" data-bs-dismiss="offcanvas">
+                            <img src="imagenes/icon_cerrar.png" style="width: 100%;">
+                        </button>
+                    </div>
+                  <h2 class="">MENU</h2>
+                </div>
+                <!-- OFF CANVAS MENU LINKS  START-->
+                <div
+                  class="offcanvas-body d-flex flex-column justify-content-between px-0 ms-4 me-4" style="width: 87%;"
+                >
+                  <ul id="desp" class="navbar-nav fs-5 justify-content-evenly">
+                    <li class="nav-item p-3 py-md-1 p-md-3">
+                      <a href="" class="nav-link pt-md-3 pb-3">Ojos</a>
+                    </li>
+                    <li class="nav-item p-3 py-md-1 p-md-3">
+                      <a href="" class="nav-link pt-md-3 pb-3">Rostro</a>
+                    </li>
+                    <li class="nav-item p-3 py-md-1 p-md-3">
+                      <a href="" class="nav-link pt-md-3 pb-3">Labios</a>
+                    </li>
+                    <li class="nav-item p-3 py-md-1 p-md-3">
+                      <a href="" class="nav-link pt-md-3 pb-3">Tools</a>
+                    </li>
+                  </ul>
+                </div>
+              </section>
+              <!-- OFFCANVAS MAIN CONTAINER END  -->
+            </div>
+        </nav>
+    </header>
+
+<section id="aten">
 <div class="container">
   <form class="customer-service-form">
     <h2>Atención al cliente</h2>
@@ -39,3 +113,6 @@ radioButtons.forEach(radio => {
   });
 });
 </script>
+<script type="text/javascript col-md-2" src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
